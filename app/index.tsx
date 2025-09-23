41618c2ec4d968ead972e8dcc254ebbe86828141
Tan <tantnse184055@fpt.edu.vn>
2025-09-23 14:16:19 +0700

    second setup

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,7 @@ import "../global.css";
 export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-950">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewinsd!</Text>
     </View>
   );
 }
