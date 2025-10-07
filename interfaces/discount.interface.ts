a5a655f3b3be104be21bb316c18d8299169f6f50
Tan <tantnse184055@fpt.edu.vn>
2025-10-07 16:35:46 +0700

    feat: implement Discount management functionality with form for creating and editing discounts

---
+++ interfaces/discount.interface.ts
@@ -0,0 +1,10 @@
+import type { components } from "@/schema-from-be";
+
+// Discount type from BE schema
+export type Discount = components["schemas"]["Discount"];
+
+// Discount create request from BE schema
+export type DiscountCreateRequest = components["schemas"]["DiscountCreateRequest"];
+
+// Discount update request from BE schema
+export type DiscountUpdateRequest = components["schemas"]["DiscountUpdateRequest"];
