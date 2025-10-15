6e44ac56a43bd1ae0ef4363539770047fcaa8788
urjhn <uyennhi01022004@gmail.com>
2025-10-15 14:30:47 +0700

    main

--- app/(onboarding)/intro-1.tsx
+++ app/(onboarding)/intro-1.tsx
@@ -15,7 +15,7 @@ export default function IntroOne() {
         <Image
           source={require("@/assets/images/intro-1.jpg")}
           style={{
-            width: 600,
+            width: 700,
             height: 600,
             borderBottomLeftRadius: 350,
             borderBottomRightRadius: 350,
